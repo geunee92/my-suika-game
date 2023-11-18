ecf58eaf66813d6e977367b6387d04d9c6d387aa
Hyungeun Jo <geunee92@ship-da.com>
2023-11-18 10:41:49 +0900

    [feat]: 과일 추가하기 함수 추가

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { Engine, Render, Runner, World, Bodies} from "matter-js"
+import { Engine, Render, Runner, World, Bodies} from "matter-js";
+import { FRUITS } from "./fruits";
 
 const engine = Engine.create();
 const render = Render.create({
@@ -31,11 +32,34 @@ const ground = Bodies.rectangle(310, 820, 620, 60, {
 
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
   isStatic: true,
+  isSensor: true,
   render: { fillStyle: "#E6B143"}
 })
 
 World.add(world, [leftWall, rightWall, ground, topLine]);
 
-
 Render.run(render);
 Runner.run(engine);
+
+let currentBody = null;
+let currentFruit = null;
+
+function addFruit() {
+  const index = Math.floor(Math.random() * 5);
+  const fruit = FRUITS[index];
+
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    index: index,
+    isSleeping: true,
+    render: {
+      sprite: { texture: `${fruit.name}.png` }
+    },
+    restitution: 0.2,
+  });
+
+  currentBody = body;
+  currentFruit = fruit;
+  World.add(world, body)
+}
+
+addFruit();
